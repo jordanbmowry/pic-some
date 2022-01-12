73d2bc19db719cde4ece14fd651c4e4dbf00565e
Jordan Mowry <jordan.mowry@gmail.com>
2022-01-12 11:07:22 -0700

    set up some basic routes with React Router v6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react"
-import Header from "./components/Header"
-import Cart from "./pages/Cart"
-import Photos from "./pages/Photos"
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Cart from './pages/Cart';
+import Photos from './pages/Photos';
 
-function App() {    
-    return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Photos />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
