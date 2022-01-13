d19eddcd874cab1488b9fa73f4cb81320ee06d03
Jordan Mowry <jordan.mowry@gmail.com>
2022-01-13 13:23:37 -0700

    created CartItem.js to display each item in on /cart route

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,21 @@
 import React, { useContext } from 'react';
 import { Context } from '../context';
+import CartItem from '../components/CartItem';
 
 function Cart() {
-  const context = useContext(Context);
-  console.log(context);
+  const { cartItems } = useContext(Context);
+  const cartItemElements = cartItems.map((item) => (
+    <CartItem key={item.id} item={item} />
+  ));
+
   return (
     <main className='cart-page'>
       <h1>Check out</h1>
+      {cartItemElements}
+      <p className='total-cost'>Total: </p>
+      <div>
+        <button>Place Order</button>
+      </div>
     </main>
   );
 }
